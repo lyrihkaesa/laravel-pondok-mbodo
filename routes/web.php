44d3d2a96f26f71d4ba845513ffd1518cbe578e4
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-04-14 01:04:09 +0700

    temp fix `ERR_TOO_MANY_REDIRECTS`, add new value `.env.example`

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,13 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('/', \App\Livewire\Home::class)->name('home');
-    Route::get('/about', \App\Livewire\About::class)->name('about');
-    Route::get('/pesantren-putra', \App\Livewire\PesantrenPutra::class)->name('pesantren-putra');
-    Route::get('/ppdb', \App\Livewire\Ppdb\Index::class)->name('ppdb.index');
-    Route::get('/ppdb/formulir', \App\Livewire\StudentRegistrationForm::class)->name('student-registration');
-    Route::get('/ppdb/biaya', \App\Livewire\Ppdb\Price::class)->name('ppdb.price');
-    Route::get('/organizations/{slug}', App\Livewire\OrganizationShow::class)->name('organizations.show');
-});
+Route::get('/', \App\Livewire\Home::class)->name('home');
+Route::get('/about', \App\Livewire\About::class)->name('about');
+Route::get('/pesantren-putra', \App\Livewire\PesantrenPutra::class)->name('pesantren-putra');
+Route::get('/ppdb', \App\Livewire\Ppdb\Index::class)->name('ppdb.index');
+Route::get('/ppdb/formulir', \App\Livewire\StudentRegistrationForm::class)->name('student-registration');
+Route::get('/ppdb/biaya', \App\Livewire\Ppdb\Price::class)->name('ppdb.price');
+Route::get('/organizations/{slug}', App\Livewire\OrganizationShow::class)->name('organizations.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
