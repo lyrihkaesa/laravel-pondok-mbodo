501dd7cf076022478bb80a08111ec7485601d6a0
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-05-11 06:57:30 +0700

    add more seeder post Blog Module #18

--- database/seeds/posts.sql
+++ database/seeds/posts.sql
@@ -13,7 +13,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Dumping data for table public.posts: 1 rows
+-- Dumping data for table public.posts: 11 rows
+DELETE FROM "posts";
 /*!40000 ALTER TABLE "posts" DISABLE KEYS */;
 INSERT INTO "posts" ("id", "author_id", "editor_id", "title", "slug", "content", "published_at", "thumbnail", "created_at", "updated_at") VALUES
 	(1, 1, 1, 'Program Keguruan', 'program-keguruan', 'Untuk menjadi tabib atau ahli hikmah yang siap praktek menangani problem-problem masyarakat, sekurang-kurangnya ada dua hal yang dimiliki. Pertama adalah bakat, kedua adalah menguasai berbagai bidang ilmu hikmah. Kami membuka bagi anda semua yang merasa punya bakat untuk melanjutkan mempelajari, menelateni berbagai asma'' ajian, suwuk, ritual untuk menjadi praktisi supranatural yang handal, layak membuka tempat praktek dengan berdiri sendiri. 
@@ -87,7 +88,76 @@ Ajian ini tanpa ada efek samping dan pantangan. Selama hafal, yakin dan istiqoma
 
 Untuk pengijazahan ditetapkan dengan mahar Rp 777.000,-
 
-Seperti biasa untuk mahar program ditransfer ke Rek BRI 601101033072534 atas nama Muchamad Ghufron. Bukti transfer bisa dikirim ke no (WA) 08569009571. ', '2024-05-11 01:15:23', 'posts/thumbnails/asma-bolo-sewu.jpg', '2024-05-10 18:15:30', '2024-05-10 18:15:30');
+Seperti biasa untuk mahar program ditransfer ke Rek BRI 601101033072534 atas nama Muchamad Ghufron. Bukti transfer bisa dikirim ke no (WA) 08569009571. ', '2024-05-11 01:15:23', 'posts/thumbnails/asma-bolo-sewu.jpg', '2024-05-10 18:15:30', '2024-05-10 22:39:34'),
+	(4, 1, 1, 'KILATAN RAMADHAN TAHUN 2024', 'kilatan-ramadhan-tahun-2024', 'Dimomen ramadhan pondok pesantren darul falah ki ageng mbodo mengadakan kajian kilatan setiap tahunnya sama seperti tahun tahun lalu ramadhan ditahun 2024 ini yayasan ponpes darul falah juga mengkaji beberapa kitab yang diantaranya:
+
+Kitab Minhajul qowwim
+Kitab Syamsul ma’arif
+Kitab Khulasoh nurul yaqin
+Kitab Arba’in nawawi
+Kitab ‘Ushfuriyyah
+Kitab Taisirul kholaq
+Kitab Washiyatul mushthofa
+Kitab Tibyan fi adabi hamalatil qur’an', '2024-03-29 05:45:53', 'posts/thumbnails/kilatan-ramadhan-tahun-2024.jpg', '2024-05-10 22:46:36', '2024-05-10 22:46:57'),
+	(5, 1, 1, 'KIRAB SANTRI MENJELANG ACARA PUNCAK HAUL AKBAR', 'kirab-santri-menjelang-acara-puncak-haul-akbar', 'Menjelang acara puncak “HAUL AKBAR KH.AHMAD BASYIR KUDUS,MBAH HAJI ABDULLAH MULYADI BIN KARDI BIN RONODIKROMO DAN MBAH HAJI AHMAD SURAT BIN MARNO KE-10 DAN WISUDA BIL GHOIB KE-3 & WISUDA BIN NADZOR KE-5” diadakan KIRAB SANTRI yang dilaksanakan pada
+
+Hari/tanggal : Selasa,5 maret 2024
+
+Tempat : Balai desa Kalongan
+
+BERIKUT BEBERAPA DOKUMENTASI :
+
+Ada beberapa pasukan kirab mulai dari Paskibraka,Drumband Al-Hawi,Santri berpakaian adat jawa,wali dan karnival,murid thoriqoh jailaniyyah,pagar nusa dan TPQ Darul Falah Ki Ageng Mbodo.Rute kirab dimulai dari depan Balai desa Kalongan  memutari Dsn.Sendangsari Mbodo sampai finish yaitu di lapangan SMP ISLAM AL-HAWI TOROH.', '2024-05-11 08:49:13', 'posts/thumbnails/kirab-santri-menjelang-acara-puncak-haul-akbar.jpg', '2024-05-10 22:49:47', '2024-05-10 22:50:09'),
+	(6, 1, 1, 'Haul Akbar Ke-10 Dan Wisuda Bil Ghoib Ke-3 & Bin Nadzor Ke-5', 'haul-akbar-ke-10-dan-wisuda-bil-ghoib-ke-3-bin-nadzor-ke-5', 'Acara dalam rangka  Haul Akbar syaikhona KH.Ahmad Basyir Kudus yang ke-10 dan Wisuda bil ghoib ke-3 & wisuda bin nadzor ke-5 pondok pesantren darul falah ki ageng mbodo yang dilaksanakan  pada:
+
+Hari: Senin s/d kamis
+
+Tanggal: 4 maret s/d 7maret 2024
+
+Tempat: Area komplek pondok pesantren darul falah ki ageng mbodo
+
+Di mana dalam acara ini panitia mengundang KH.AHFAS FAISHOL HAMID BAIDHOWI dan turut mengundang SYAIKH MUHAMMAD ‘ATHIF  min AL-AZHAR KAIRO MESIR', '2024-03-17 05:51:06', 'posts/thumbnails/haul-akbar-ke-10-dan-wisuda-bil-ghoib-ke-3-bin-nadzor-ke-5.jpg', '2024-05-10 22:51:39', '2024-05-10 22:51:39'),
+	(7, 1, 1, 'Pesta Demokrasi Santri Pondok Mbodo', 'pesta-demokrasi-santri-pondok-mbodo', 'Pesta Demokrasi rakyat telah selesai, saatnya pesta demokrasi santri Pondok Mbodo. Dikatakan pesta demokrasi karena Pondok Pesantren Darul falah Ki Ageng Mbodo mengadakan PEMILU (Pemilihan Umum) untuk pemilihan Presiden BES (Badan Eksekutif Santri) dan Ketua DPS (Dewan Perwakilan Santri) beserta anggotanya pada periode jabatan 2020/ 2022. Pemilihan yang perdana ini dilaksanakan pada malam Minggu, 12 Desember 2020.
+
+Untuk menunjukkan kesungguhan dan Visi Misi dari para calon, kegiatan ini dimulai dengan debat terbuka calon dan santri Pondok Mbodo. Dalam debat ini para calon diharuskan dapat menjawab pertanyaan-pertanyaan yang dilontarkan baik dari pihak Pannelis, santri, maupun para calon sendiri. Perdebatan berlangsun lancar dan meriah, masing-masing kandidat tak mau kalah dalam memaparkan kesanggupanya untuk memimping dan mengelola Pesantren Darul Falah kedepannya.
+
+Tibalah saat yang ditunggu yakni pemilihan langsung, dalam pemilihan perdana ini santri yang memiliki hak suara diantaranya keluarga ndalem, kelas XII SMK Al Hawi, Mutakhorijin Jurusan, Santri Senior, dan santri jenjang akhir jurusan. Perhitungan suara yang diperoleh menghasilkan bahwa Presiden BES periode 2020/ 2022 dijabat oleh Badawi Jamil dengan perolehan suara 27 dari 32 suara yang sah. sedangkan Ketua DPS periode 2020/ 2022 dijabat oleh Thoriq Ilham Romadhon denagn perolehan suara 22 dari 32 suara yang sah. Dialnjutkan dengan pembentukan kepengurusan melalui sidang bersama Yayasan dan Tim Formatur
+
+Pesiden BES, Ketua DPS beserta anggota-anggotanya dilantik oleh Yayasan dengan pengambilan sumpah jabatan dan Ikrar Astabrata dengan khidmat dan kesungguhan. Demikianlah BES dan DPS dibentuk sebagai badan pelaksana program-program pesantren dalam naungan dan bimbingan Yayasan Pondok Pesantren Darul Falah Ki Ageng Mbodo.', '2024-02-13 05:52:10', 'posts/thumbnails/pesta-demokrasi-santri-pondok-mbodo.jpg', '2024-05-10 22:52:54', '2024-05-10 22:52:54'),
+	(8, 1, 1, 'Nasehat Untuk Santri Dalam ”Pepali Ki Ageng Mbodo”', 'nasehat-untuk-santri-dalam-pepali-ki-ageng-mbodo', 'Untuk bisa mencetak santri yang cerdas intelektual, sosial, dan spiritual, PP Darul falah Ki Ageng Mbodo memiliki ajaran – ajaran dasar bagaimana santri harus berlaku yang dituangkan dalam syiir ” Pepali Ki Ageng Mbodo” sebagai berikut :
+
+Lelakune santri naliko luru ilmu njiret weteng, nyengkal moto, dzikir, lan puoso. Sregep nderes Al Qur’an sregep wiridan sregep apalan sedoyo pelajaran. Apal iku setengahe faham lan nguasani ilmu.
+
+Menjadi seorang santri harus tahan tirakat untuk bisa menguasai ilmu yang diberikan. Tirakat disini seperti puasa, dzikir, membaca Al Qur’an, wiridan, dan menghafalkan pelajaran.
+Ngertenono santri ilmu iku koyok jaran ditaleni dicancang supoyo ora ilang. Ditulis kanthi setiti, dimaknani sing rapi dideres rino wengi supoyo mlebu ati. Nak wis manjing dilakoni dadi ilmu kang manfaati.
+
+Ilmu yang dimiliki seorang santri bisa merasuk kedalam hati menjadi ilmu yang bermanfaat setelah santri itu mau mencatatnya, mempelajari siang dan malam kemudian mengamalkannya dalam kehidupan sehari – hari.
+Gunakno toto kromo tumprap sopo wae lisan alus, sikap alus, budi kang utomo. Takdim marang guru patuh opo dawuhe khusnudzon marang perintahe dadi berkah marang ilmune. Guru iku sumbere wahyu nyambung marang kanjeng nabi.
+
+Seorang santri harus memiliki tata krama terhadap siapapun, perkataan yang halus, sikap yang halus, budi pekerti yang luhur, dan patuh terhadap guru. Sehingga ilmu yang dimiliki mendatangkan barokah. Ibarat kata, guru itu penyambung tali santri terhadap Nabi Muhammad SAW.
+Jujur andhap aspr iku sifat dadi santri ojo sombong ojo kemaki niru akhlaq kanjeng nabi. Puncake ilmu laku kang pgrasojo welas asih marang liyo wicaksono ing tulodho. Ilmu iku dadi uyahe, akhlaq iku dadi tepunge.
+
+Seorang santri harus memiliki sifat rendah hati, jujur, perilaku bijaksana, mengasihi terhadap sesama sebagaimana yang telah dicontohkan Nabi Muhammad SAW. Ilmu tanpa akhlaq bisa mendatangkan petaka.
+Lungamu budhal ngaji oleh restu tiyang sepuh diwanti – wanti hasile ojo nguciwakke. Ojo mung tura turu lan ojo mung dolan wae ayo sregep ngajine ojo nglokro semangate. Ilmu iku kanthi sinau ora mung tura turu.
+
+Ketika santri menuntut ilmu dan mendapat restu orang tua diharapkan ketika pulang nanti membawa hasil yang tidak mengecewakan. Jadi yang lebih rajin belajarnya dan jangan patah semangat.
+ 
+
+Begitulah tadi sekelumit uraian bagaimana santri belkau yang bisa diandalkan untuk menjaga keutuhan NKRI, serta menjadi tongkat penerus dari Ulama’.', '2024-02-12 05:53:33', 'posts/thumbnails/nasehat-untuk-santri-dalam-pepali-ki-ageng-mbodo.jpg', '2024-05-10 22:54:12', '2024-05-10 22:54:12'),
+	(9, 1, 1, 'Haul Akbar Ke-8 Dan Wisuda Qur’an Bil Ghoib Ke-1 & Qur’an Bin Nadzor Ke-3', 'haul-akbar-ke-8-dan-wisuda-quran-bil-ghoib-ke-1-quran-bin-nadzor-ke-3', 'Acara dalam rangka Haul Akbar Syaikhona KH. Ahmad Basyir, Simbah Mulyadi Bin Kardi Ronodikromo, Simbah Surat bin Marno ke- 8, Muslimin-Muslimat dan Wisuda Qur’an Bil Ghoib ke-1 & Wisuda Qur’an Bin Nadzor ke-3.
+Yang dilaksanakan kemarin pada :
+
+Hari : Kamis s/d Sabtu
+Tanggal : 17 s/d 19 Maret 2022
+Tempat : Kompleks Yayasan Pondok Pesantren Darul Falah Ki Ageng Mbodo
+
+Dan berikut adalah hasil dokumentasi dari acara diatas…', '2022-03-25 05:55:04', 'posts/thumbnails/haul-akbar-ke-8-dan-wisuda-quran-bil-ghoib-ke-1-quran-bin-nadzor-ke-3.jpg', '2024-05-10 22:56:14', '2024-05-10 22:56:14'),
+	(10, 1, 1, 'Warnai Bulan Maulud Dengan Lomba, Asah Skill Santri', 'warnai-bulan-maulud-dengan-lomba-asah-skill-santri', 'Bulan Maulud telah tiba, seperti adat santri dan masyarakat pada umumnya yaitu melaksanakan Mauludan selama 14 hari. Hal ini dilakukan karena rasa bersyukur dan bahagianya umat manusia karena kelahiran Nabi Muhammad SAW. Tak terlepas juga bagi santri Pondok Pesantren Ki Ageng Mbodo. Tak hanya melaksanakan maulidan selama 14 hari, namun di Pondok Mbodo ini juga mengadakan perlombaan antar jurusan baik perlombaan dari segi kecerdasan otar hingga perlombaan fisik/ olahraga. Perlombaan yang dilakukan secara jurusan, dalam arti santri akan sangat bekerja keras, belajar tekun dan berusaha yang terbaik untuk memenangkan perlombaan atas nama jurusan mereka masing – masing. Beberapa lomba tersebut adalah lomba cerdas cermat, lalaran alfiyah, badminton, volly layar, volly air/ taplak, futsal, lari sprin, dan simulasi nganten. Lomba cerdas cermat disini santri diharuskan mempelajari materi dari 5 jurusan yang ada yakni Persiapan jurusan, Tahsin, Tahfidz, Kitab, dan Suwuk. Karena setiap delegasi dari masing – masing jurusan akan mendapatkan sekurang kurangnya 5 pertanyaan dari setiap jurusan.  Dalam lomba lalaran Alfiyyah sendiri setiap jurusan wajib menadhomkan alfiyah dengan nada yang berbeda jika terjadi kesamaan maka akan dikenakan diskualifikasi.', '2020-11-18 05:57:42', 'posts/thumbnails/warnai-bulan-maulud-dengan-lomba-asah-skill-santri.jpg', '2024-05-10 22:58:05', '2024-05-10 22:58:05'),
+	(11, 1, 1, 'Karantina, Santri Tetap Semangat Ngaji Dan Bersholawat', 'karantina-santri-tetap-semangat-ngaji-dan-bersholawat', 'Dalam situasi merebaknya Virus Covid-19 dimana kebanyakan pesantren di Indonesia dipulangkan namun berbeda dengan Pondok Mbodo yang  tetap mengadakan kajian kepesantrenan dengan sistem sementara yakni ” Karantina Pesantren”. Dalam kegiatan karantina ini santri diberikan kegiatan yang berbeda dengan kegiatan sehari – hari pesantren yang lebih menekankan pada pendidikan formal dan kajian kitab. Diantara kegiatan dalam Karantina Pesantren ini antara lain Bercocok Tanam, Outbond, Olahraga Pagi, beberapa kajian kitab sederhana yang dipandu langsung oleh Abah K. M. Ghufron ( Pengasuh ), serta kegiatan Belajar Sambil Bermain yang tentunya tidak merusak psikis dan sistem imun para santri.
+
+Salah satu kegiatan rutin Pondok Pesantren Darul Falah Ki Ageng Mbodo adalah Majelis lapanan NKRI yang dilaksanakan satu kali setiap bulannya. Namun pada bulan ini dikarenakan situasi yang tidak memungkinkan, Pondok Mbodo tetap melaksanakan Majelis NKRI namun tanpa jamaah dari luar. Hal ini telah disepakati dengan Maklumak Pesantren yang melarang tamu, wali santri, maupun orang luar berkunjung sampai batas waktu yang ditentukan. Kegiatan ini berlangsung pada malam Minggu Kliwon, 18 April 2020 dan bertempat di Maqam Mbah Mulyadi. Dengan rangkaian acara yang sama, namun sedikit berbeda saat melantunkan sholawat yang diiringi dengan musik Rebana. Dipimpin langsung oleh Abah K. M. Ghufron Mulyadi, para santri sangat bersemangat melantunkan sholawat dengan menggerakkan tangan yang bertujuan untuk menggapai kesempurnaan sholawat kepada Nabi Muhammad SAW.
+
+Abah K. M. Ghufron Mulyadi juga mengungkapkan bahwa santri yang masih di Pondok Mbodo saat kegiatan ini berlangsung merupakan santri yang beruntung, karena ditengah merebaknya Virus Covid-19 jarang sekali orang – orang dapat bersholawat dengan suka cita seperti santri Pondok Mbodo pada malam ini. Demikianlah kegiatan Karantina Pesantren yang mengasikkan dan akan disusul dengan Kegiata Kilatan Ramadan beberapa hari lagi.', '2020-04-19 05:59:40', 'posts/thumbnails/karantina-santri-tetap-semangat-ngaji-dan-bersholawat.png', '2024-05-10 23:00:10', '2024-05-10 23:00:10');
 /*!40000 ALTER TABLE "posts" ENABLE KEYS */;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
