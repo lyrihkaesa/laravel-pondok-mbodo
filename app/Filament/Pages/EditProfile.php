d6b82d2e13061ab8bc3ed8d035ccc630dac61c5e
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-05-13 02:48:31 +0700

    feat: Profile Module #20 #1

---
+++ app/Filament/Pages/EditProfile.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Exception;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Pages\Page;
+use Filament\Actions\Action;
+use Filament\Facades\Filament;
+use Illuminate\Support\Facades\Hash;
+use Filament\Forms\Contracts\HasForms;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Notifications\Notification;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Filament\Forms\Concerns\InteractsWithForms;
+
+class EditProfile extends Page implements HasForms
+{
+    use InteractsWithForms;
+    // protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static bool $shouldRegisterNavigation = false;
+
+    protected static string $view = 'filament.pages.edit-profile';
+
+    public ?array $profileData = [];
+    public ?array $passwordData = [];
+
+    public function mount(): void
+    {
+        $this->fillForms();
+    }
+
+    protected function getForms(): array
+    {
+        return [
+            'editProfileForm',
+            'editPasswordForm',
+        ];
+    }
+    public function editProfileForm(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('Profile Information')
+                    ->description('Update your account\'s profile information and email address.')
+                    ->aside()
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required(),
+                        Forms\Components\TextInput::make('email')
+                            ->email()
+                            ->required()
+                            ->unique(ignoreRecord: true),
+                    ]),
+            ])
+            ->model($this->getUser())
+            ->statePath('profileData');
+    }
+
+    public function editPasswordForm(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('Update Password')
+                    ->description('Ensure your account is using long, random password to stay secure.')
+                    ->aside()
+                    ->schema([
+                        Forms\Components\TextInput::make('Current password')
+                            ->password()
+                            ->required()
+                            ->currentPassword(),
+                        Forms\Components\TextInput::make('password')
+                            ->password()
+                            ->required()
+                            ->rule(Password::default())
+                            ->autocomplete('new-password')
+                            ->dehydrateStateUsing(fn ($state): string => Hash::make($state))
+                            ->live(debounce: 500)
+                            ->same('passwordConfirmation'),
+                        Forms\Components\TextInput::make('passwordConfirmation')
+                            ->password()
+                            ->required()
+                            ->dehydrated(false),
+                    ]),
+            ])
+            ->model($this->getUser())
+            ->statePath('passwordData');
+    }
+
+    protected function getUser(): Authenticatable & Model
+    {
+        $user = Filament::auth()->user();
+        if (!$user instanceof Model) {
+            throw new Exception('The authenticated user object must be an Eloquent model to allow the profile page to update it.');
+        }
+        return $user;
+    }
+
+    protected function fillForms(): void
+    {
+        $data = $this->getUser()->attributesToArray();
+        $this->editProfileForm->fill($data);
+        $this->editPasswordForm->fill();
+    }
+
+    protected function getUpdateProfileFormActions(): array
+    {
+        return [
+            Action::make('updateProfileAction')
+                ->label(__('filament-panels::pages/auth/edit-profile.form.actions.save.label'))
+                ->submit('editProfileForm'),
+        ];
+    }
+
+    protected function getUpdatePasswordFormActions(): array
+    {
+        return [
+            Action::make('updatePasswordAction')
+                ->label(__('filament-panels::pages/auth/edit-profile.form.actions.save.label'))
+                ->submit('editPasswordForm'),
+        ];
+    }
+
+    public function updateProfile(): void
+    {
+        $data = $this->editProfileForm->getState();
+        $this->handleRecordUpdate($this->getUser(), $data);
+        $this->sendSuccessNotification();
+    }
+
+    public function updatePassword(): void
+    {
+        $data = $this->editPasswordForm->getState();
+        $this->handleRecordUpdate($this->getUser(), $data);
+        if (request()->hasSession() && array_key_exists('password', $data)) {
+            request()->session()->put(['password_hash_' . Filament::getAuthGuard() => $data['password']]);
+        }
+        $this->editPasswordForm->fill();
+        $this->sendSuccessNotification();
+    }
+
+    private function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $record->update($data);
+        return $record;
+    }
+
+    private function sendSuccessNotification(): void
+    {
+        Notification::make()
+            ->success()
+            ->title(__('filament-panels::pages/auth/edit-profile.notifications.saved.title'))
+            ->send();
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,22 +2,23 @@
 
 namespace App\Providers\Filament;
 
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
-use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Filament\PanelProvider;
+use Filament\Navigation\MenuItem;
+use Filament\Support\Colors\Color;
+use App\Filament\Pages\EditProfile;
+use Filament\Http\Middleware\Authenticate;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
 
 class AdminPanelProvider extends PanelProvider
@@ -30,6 +31,10 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile()
+            ->userMenuItems([
+                'profile' => MenuItem::make()->url(fn (): string => EditProfile::getUrl()),
+            ])
             ->colors([
                 // 'primary' => Color::Lime,
                 // 'pink' => Color::Pink,
