7c1958a5461c34f54c63873e863deef5cd762f05
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-05-15 16:24:50 +0700

    add: `getHeaderActions()` in `ViewEmployee` #14

--- app/Filament/Resources/EmployeeResource/Pages/ViewEmployee.php
+++ app/Filament/Resources/EmployeeResource/Pages/ViewEmployee.php
@@ -2,13 +2,27 @@
 
 namespace App\Filament\Resources\EmployeeResource\Pages;
 
-use App\Filament\Resources\EmployeeResource;
 use Filament\Actions;
+use Illuminate\Database\Eloquent\Model;
 use Filament\Resources\Pages\ViewRecord;
+use App\Filament\Resources\EmployeeResource;
 
 class ViewEmployee extends ViewRecord
 {
     protected static string $resource = EmployeeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+            Actions\Action::make('goToViewStudentAction')
+                ->label(__('Student'))
+                ->icon('icon-student-male')
+                ->visible(fn (Model $record) => $record->user->student !== null)
+                ->url(fn (Model $record) => route('filament.admin.resources.students.view', $record->user->student))
+                ->openUrlInNewTab(),
+        ];
+    }
 
     protected function mutateFormDataBeforeFill(array $data): array
     {
