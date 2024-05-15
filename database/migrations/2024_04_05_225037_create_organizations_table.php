05728c3475c9a296dd72bd92205c7bd0e20cf9ec
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-05-15 17:30:31 +0700

    add: `SoftDeletes` in Organization Module #9

--- database/migrations/2024_04_05_225037_create_organizations_table.php
+++ database/migrations/2024_04_05_225037_create_organizations_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
