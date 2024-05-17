cb629d19a134f283e5d26bd7ac7156023781ba4a
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-05-17 18:29:38 +0700

    remove unused files `Livewire` `About` `Home` #19

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,15 @@ Route::get('/ppdb/formulir', \App\Livewire\Ppdb\Registration::class)->name('stud
 Route::get('/ppdb/biaya', \App\Livewire\Ppdb\Price::class)->name('ppdb.price');
 
 Route::get('/peraturan', \App\Livewire\Law\Index::class)->name('law.index');
+
 Route::get('/lembaga/{slug}', \App\Livewire\Organization\Show::class)->name('organizations.show');
 
 Route::get('/blog', \App\Livewire\Post\Index::class)->name('posts.index');
 Route::get('/blog/{slug}', \App\Livewire\Post\Show::class)->name('posts.show');
 
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-Route::get('/admin/financial-transactions/pdf', [FinancialTransactionController::class, 'generatePdfReport'])->name('admin.financial-transactions.pdf');
+Route::middleware('auth')->group(function () {
+    Route::get('/admin/financial-transactions/pdf', [FinancialTransactionController::class, 'generatePdfReport'])->name('admin.financial-transactions.pdf');
+});
 
 Route::get('/pdf', function () {
     return view('reports.pdf_financial_transactions_v2', [
