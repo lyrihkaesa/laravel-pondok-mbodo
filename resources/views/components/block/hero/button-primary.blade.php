c53e66904268a6634d0c43950ea2ee889c9e5e54
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-06-01 13:52:40 +0700

    chore: attributes `twMerge()` #27

--- resources/views/components/block/hero/button-primary.blade.php
+++ resources/views/components/block/hero/button-primary.blade.php
@@ -1,7 +1,5 @@
-<a {!! $attributes->merge([
-    'class' =>
-        'inline-flex items-center justify-center gap-x-2 rounded-lg border border-transparent bg-teal-600 px-4 py-3 text-sm font-semibold text-white hover:bg-teal-700 disabled:pointer-events-none disabled:opacity-50',
-]) !!}>
+<a
+    {{ $attributes->twMerge(['class' => 'inline-flex items-center justify-center gap-x-2 rounded-lg border border-transparent bg-teal-600 px-4 py-3 text-sm font-semibold text-white hover:bg-teal-700 disabled:pointer-events-none disabled:opacity-50']) }}>
     {{ $slot }}
     <svg class="size-4 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
