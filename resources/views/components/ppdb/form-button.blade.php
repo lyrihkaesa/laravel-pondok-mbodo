c6fa26ab7dccde09f4a2b3f4a09260bfb46a731c
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-06-11 14:52:20 +0700

    fix: change spa form ppdb to default SPA cause css #30 #11

--- resources/views/components/ppdb/form-button.blade.php
+++ resources/views/components/ppdb/form-button.blade.php
@@ -1,4 +1,4 @@
-<a wire:navigate href="{{ route('student-registration') }}"
+<a href="{{ route('student-registration') }}"
     {{ $attributes->twMerge(['class' => 'whitespace-nowrap py-3 px-4 sm:inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-teal-500 text-white hover:bg-teal-600 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600']) }}
     type="button">
     Pendaftaran Online
