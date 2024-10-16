8b740b5da353fc7515e8143d7c1d9ff88a1bc5c1
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2024-10-16 11:11:15 +0700

    fix: redirect login page to filament #34

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', \App\Livewire\Page::class)->name('home');
 Route::get('/tentang', \App\Livewire\Page::class)->name('about');
 
+Route::get('/login', function () {
+    return redirect()->route('filament.admin.auth.login');
+})->name('login');
+
 Route::get('/ppdb', \App\Livewire\Page::class)->name('ppdb.index');
 Route::get('/ppdb/formulir', \App\Livewire\Ppdb\Registration::class)->name('student-registration');
 Route::get('/ppdb/biaya', \App\Livewire\Page::class)->name('ppdb.price');
@@ -40,6 +44,6 @@ Route::get('/pdf', function () {
     ]);
 })->name('pdf');
 
-require __DIR__ . '/auth.php';
+// require __DIR__ . '/auth.php';
 
 Route::fallback(\App\Livewire\Page::class);
